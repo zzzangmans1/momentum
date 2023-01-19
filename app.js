bd0aa9d1dcc2de09f9ddf9a91c42b65b173f835f
leewonjoo <dnjswnzzang@gmail.com>
2023-01-19 18:23:47 +0900

    event 1

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 // document 는 html 전체를 의미
 let hello = document.getElementById("title")
 hello.style.color = "blue";
+
+function handleTitelClick() {
+    console.log("click")
+}
+
+hello.addEventListener("click", handleTitelClick)
