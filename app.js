e137bd81dab1f6d87ae6ad211350e068d1a89958
leewonjoo <dnjswnzzang@gmail.com>
2023-01-20 21:00:55 +0900

    login #3

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const login = document.querySelector("#login-form");
+const loginInput = login.querySelector("input")
 
 
-function handleLoginBtnClick() {
-    if(loginInput.value !== ""){
-        console.log(loginInput.value);
-        loginInput.placeholder = "";
-    }
+function onLoginSubmit(event) {         // js는 첫 매개볁수는 이벤트 오브젝트를 넘긴다.
+    event.preventDefault();             // 브라우저가 기본 동작을 실행하지 못하게 막기
+    console.log(loginInput.value);     
 }
 
-loginButton.addEventListener("click", handleLoginBtnClick);
+login.addEventListener("submit", onLoginSubmit);    // submit 같은 경우에는 form에서 submit 이벤트를 캐치해야 한다?
