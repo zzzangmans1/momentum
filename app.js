1b4bef8cfd7509088797a78c72eac79f3102f9d4
leewonjoo <dnjswnzzang@gmail.com>
2023-01-20 20:02:38 +0900

    login #1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// document 는 html 전체를 의미
-let h1 = document.querySelector(".sexy-font")
-console.log(h1)
-// 이벤트를 보고 싶으면 console.dir 로  확인한다.
-function handleTitelClick() {   // classList 는 className 여러개를 사용할 때 사용
-    h1.classList.toggle("active")
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+
+function handleLoginBtnClick() {
+    if(loginInput.value !== ""){
+        console.log(loginInput.value);
+        loginInput.placeholder = "";
+    }
 }
-h1.addEventListener("click", handleTitelClick)
+
+loginButton.addEventListener("click", handleLoginBtnClick);
