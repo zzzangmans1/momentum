acfda915a661e3342bedcd6b5e665fc47e09a9df
leewonjoo <dnjswnzzang@gmail.com>
2023-01-25 19:11:27 +0900

    todo # 2

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li); 
+}
+
 function handleToDoSubmit(evnet){
     evnet.preventDefault();
-    const newValue = toDoInput.value
-    toDoList.innerText = newValue;
+    const newToDo = toDoInput.value
     toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
