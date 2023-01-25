a0a93d2f4d0b5ffb7a46f61c912d6b337bec95aa
leewonjoo <dnjswnzzang@gmail.com>
2023-01-25 18:29:54 +0900

    quotes # 1

--- js/clock.js
+++ js/clock.js
@@ -4,7 +4,7 @@ function setClock(){
     const date = new Date();
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");     // padStart 는 1. 전체 길이 2. 패딩에 넣을 문자 그리고 Start는 앞에서부터 시작 End는 뒤에서부터 시작
 
     clock.innerText = `${date.getFullYear()} / ${date.getMonth() + 1} / ${date.getDate()} ${hours}:${minutes}:${seconds}`
 }
